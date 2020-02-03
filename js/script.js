60f85f1852ffb6534f8669876d19c6f32020a60f
Assis Ferreira <assisrunny@hotmail.com>
2020-02-03 16:38:10 -0100

    Fill and fixed positions

--- js/script.js
+++ js/script.js
@@ -7,6 +7,6 @@ $(document).ready(function(){
 	for(var l = 0; l < Ska.supportedObjects.length; l++)
 	{
 		$("#object-list")
-		.append('<div class="col-12 col-sm-3 col-md-2"><div data-ska-object="' + Ska.supportedObjects[l].name + '"></div><p><b>' + Ska.supportedObjects[l].name + '</b></p><p class="small">data-ska-object="' + Ska.supportedObjects[l].name + '"</p></div>');
+		.append('<div class="col-6 col-sm-3 col-md-2"><div data-ska-object="' + Ska.supportedObjects[l].name + '"></div><p><b>' + Ska.supportedObjects[l].name + '</b></p><p class="small">data-ska-object="' + Ska.supportedObjects[l].name + '"</p></div>');
 	}
 });
